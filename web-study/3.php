4eb2735e5e779298cefcb34c9f14119af0b63a11
Longinushadow <43020577+Longinushadow@users.noreply.github.com>
2020-08-20 23:09:26 +0800

    画图文字
    123

---
+++ web-study/3.php
@@ -0,0 +1,19 @@
+<?php
+header('content-type:image/gif');
+$image=imagecreatetruecolor(500,500);
+$red=imagecolorallocate($image,255,0,0);
+$green=imagecolorallocate($image,0,255,0);
+$blue=imagecolorallocate($image,0,0,255);
+$white=imagecolorallocate($image,255,255,255);
+imagefill($image,0,0,$white);
+$font=realpath('Alibaba-PuHuiTi-Bold.ttf');
+$text='稻草人';
+for ($i=0; $i <mb_strlen($text); $i++) { 
+    imagettftext(
+        $image,20,mt_rand(0,30),25*$i+50,50,$red,$font,mb_substr
+        ($text,$i,1,'utf-8'));
+
+}
+imagepng($image);
+imagedestroy($image);
+?>
