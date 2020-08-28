dd1721708fcfc951447df151ca9746ffa760743c
Longinushadow <43020577+Longinushadow@users.noreply.github.com>
2020-08-28 17:25:48 +0800

    水印处理

--- web-study/6.php
+++ web-study/6.php
@@ -1,6 +1,9 @@
 <?php
 include 'water.php';
+try{
 $water=new Water;
-$water->make('2.png');
-
+$water->make('5.png','3.png');
+}catch (Exception $e){
+    echo $e->getMessage();
+}
 ?>
--- web-study/water.php
+++ web-study/water.php
@@ -1,10 +1,27 @@
 <?php
 class Water
 {
-    public function make(string $image)
-    {
+    public function make(string $image,string $filename=null)
+    {   
+        $this->checkImage($image);
         $res= $this->resource($image);
-        var_dump($res);
+        // $info=getimagesize($image);
+        // print_r($info);
+        return $this->showAction($image)($res,$filename??$image);
+        // var_dump($res);
+    }
+    protected function checkImage(string $image)
+    {
+        if(!is_file($image) || getimagesize($image)===false){
+            throw new Exception('file is not image or file is not existed');
+        }
+    }
+    protected function showAction(string $image)
+    {
+        $info=getimagesize($image);
+        $functions=[1=>'imagegif',2=>'imagejpeg',3=>'imagepng'];
+        return $functions[$info[2]];
+
     }
     protected function resource(string $image)
     {
