6b0e6bc5999e73ccb3a3a5558bbac98ef63f8a61
Longinushadow <43020577+Longinushadow@users.noreply.github.com>
2020-09-08 22:29:20 +0800

    缩减图

--- web-study/缩略图/2.php
+++ web-study/缩略图/2.php
@@ -1,1 +1,9 @@
 <?php
+include 'Thumb.php';
+$thumb=new Thumb;
+try{
+$thumb->make('picture2.jpg',400,500);
+}catch (Exception $e){
+    echo $e->getMessage();
+}
+?>
---
+++ web-study/缩略图/Thumb.php
@@ -0,0 +1,35 @@
+<?php
+class Thumb{
+    public function make(string $image,int $width,int $height){
+        $image = $this->resource($image);
+        $info = $this->size($width, $height, imagesx($image), imagesy($image));
+        $res = imagecreatetruecolor($info[0], $info[1]);
+        imagecopyresampled($res, $image, 0, 0, 0, 0, $info[0], $info[1], $info[2], $info[3]);
+        header('Content-type:image/jpeg');
+        imagejpeg($res);
+    }
+    protected function size($rw,$rh,$iw,$ih)    
+    {
+        if($iw/$rw>$ih/$rh){
+            $iw =$ih/$rh*$rw;
+        }else{
+            $ih =$iw/$rw*$rh;
+        }
+        return [$rw,$rh,$iw,$ih];
+    }
+    protected function checkImage(string $image){
+        if (!is_file($image) || getimagesize($image)===false) {
+            throw new Exception("file is not exists or it is not iamge"); 
+        }
+    }
+    protected function resource(string $image){
+         $this->checkImage($image);
+         $info=getimagesize($image);
+         $functions=[1=>'imagecreatefromgif',2=>'imagecreatefromjpeg',3=>'imagecreatefrompng'];
+         $call = $functions[$info[2]];
+         return $call($image);
+    }
+}
+
+
+?>
