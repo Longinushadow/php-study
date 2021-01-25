d391fb0aab05a4860bbb89ae89fc1a5195eba0fa
Longinushadow <43020577+Longinushadow@users.noreply.github.com>
2021-01-25 17:47:14 +0800

    生成器中完成添加和查询

---
+++ web-study/数据库操作/5.php
@@ -0,0 +1,25 @@
+<?php
+use DateBase\DB;
+include 'DB.php';
+$config=[
+    'host'=>'localhost',
+    'user'=>'root',
+    'password'=>'root',
+    'database'=>'mysql',
+    'charset'=>'utf8'
+];
+
+try{
+    $new=new DB($config);
+    // $rows=$new->query("Insert into study_test (title,name,sex) values(:title,:name,:sex)",[':title'=>'第7个',':name'=>'DioDio',':sex'=>'女']);
+    // $rows=$new->query_prepare('select * from study_test where id>=? ',['1']);
+    // print_r($rows);
+    // $rows=$new->table('study_test')->field('id','name','title')->order(' id')->limit(' 1')->get();
+    // print_r($rows);
+    $rows=$new->table('study_test')->insert(['title'=>"第九个",'name'=>"jiujiu",'sex'=>"女"]);
+    print_r($rows);
+
+}catch(Exception $e){
+    die($e->getMessage());
+}
+?>
--- web-study/数据库操作/DB.php
+++ web-study/数据库操作/DB.php
@@ -5,6 +5,8 @@ use Exception;
 use PDO;
 
 class DB{
+    protected $link;
+    protected $options=['table'=>'','field'=>' * ','order'=>'','limit'=>'','where'=>''];
     public function __construct(array $config)
     {
         $this->connect($config);
@@ -14,14 +16,58 @@ class DB{
              $config['host'],$config['database'],$config['charset']);
         $this->link=new PDO($dsn,$config['user'],$config['password'],[PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC]);   
     }
-    public function query_prepare(string $sql,array $vars){
+    //查询
+    public function query_prepare(string $sql,array $vars=[]){
         $sth=$this->link->prepare($sql);
         $sth->execute($vars);
-        print_r($sth->fetchAll()) ;
+        return $sth->fetchAll();
     }
+    //插入/更新/删除
     public function query(string $sql,array $vars){
         $sth=$this->link->prepare($sql);
         return $sth->execute($vars);
+    }
+    public function table(string $table){
+          $this->options['table']=$table;
+        //   print_r($this->options);
+          return $this;
+    }
+    public function field(...$fields){
+        $this->options['field']='`'.implode('`,`',$fields).'`';
+        // print_r($this->options);
+        return $this;
+    }
+    public function limit(...$limit){
+        $this->options['limit']="limit".implode(',',$limit);
+        // print_r($this->options);
+        return $this;
+    }
+    public function order(string $order){
+        $this->options['order']="order by".$order;
+        // print_r($this->options);
+        return $this;
+    }
+    public function where(string $where){
+        $this->options['where']=" where".$where;
+        // print_r($this->options);
+        return $this;
+    }
+    public function get(){
+        $sql="SELECT {$this->options['field']} from {$this->options['table']} {$this->options['where']}
+        {$this->options['order']} {$this->options['limit']}";
+        // print_r($sql);
+        return $this->query_prepare($sql);   
+    }
+    public function insert(array $vars){
+        //insert into study_test () values ();
+        $fields='`' . implode('`,`',array_keys($vars)) . '`';
+        $values=implode(',',array_fill(0,count($vars),'?'));
+        $sql="INSERT INTO {$this->options['table']} ($fields) values ($values)";
+        return $this->query_prepare($sql,array_values($vars));
+        // print_r($sql);
+        // print_r(array_values($vars));
+        // die($sql);
+        
     }
 }
 
